82fc1c907aa0d11306b89b96ffc55d61d36c38d1
KJP952 <parkkatie2003@gmail.com>
2024-08-31 14:29:14 -0400

    commit

---
+++ app/api/chat/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+export async function POST(req) {
+
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-flash",
+    systemInstruction: "Ai Chat Bot",
+    });
+
+  try {
+    
+    const data = await req.json();
+
+    // Construct the conversation history:
+    const conversationHistory = data.map(message => {
+        return `${message.role}: ${message.content}`;
+      }).join("\n\n");
+    
+
+    // Combine the system instruction with the conversation history 
+    const prompt = `${model.systemInstruction}\n\nHere's what has been discussed so far:\n${conversationHistory}\n`;
+
+    // Send user's prompt and then get assistant's response:
+    const result = await model.generateContentStream(prompt);
+    const response = await result.response;
+    const text = await response.text();
+
+    // Return the assistant's response:
+    return new Response(text);
+
+
+  } catch (error) {
+    console.error("Error in API Call:", error.message);
+    console.error("Full Error Details:", error);
+    return NextResponse.json({ error: "Error generating response" }, { status: 500 });
+  }
+}
